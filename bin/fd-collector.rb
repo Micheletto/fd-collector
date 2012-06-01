169168153468e50fa07a8e53a09363a7e01b2cb7
Robert Micheletto <bobm@mozilla.com>
2012-06-01 09:24:50 -0700

    Fixed tabs, changed Main loop

--- bin/fd-collector.rb
+++ bin/fd-collector.rb
@@ -13,21 +13,21 @@ options = {}
 
 # Parse command line options.
 optparse = OptionParser.new do |opts|
-	opts.banner = "Usage: [ -i SECONDS -a APPLICATION ] DIRECTORY"
+    opts.banner = "Usage: [ -i SECONDS -a APPLICATION ] DIRECTORY"
 
-	options[:interval] = 20
-	opts.on( '-i', '--interval SECONDS', "Seconds between checks.") do |sec|
-		options[:interval] = sec
-	end
+    options[:interval] = 20
+    opts.on( '-i', '--interval SECONDS', "Seconds between checks.") do |sec|
+        options[:interval] = sec
+    end
 
-	opts.on( '-a', '--application APPLICATION', "Name of the application.") do |a|
-		options[:application] = a
-	end
+    opts.on( '-a', '--application APPLICATION', "Name of the application.") do |a|
+        options[:application] = a
+    end
 
-	opts.on( '-h', '--help', "Display help information.") do 
-		puts opts
-		exit
-	end
+    opts.on( '-h', '--help', "Display help information.") do 
+       puts opts
+       exit
+    end
 end
 
 # Make it happen.
@@ -35,13 +35,13 @@ optparse.parse!
 
 # Make sure a directory was supplied.
 unless ARGV[0]
-	raise OptionParser::MissingArgument
+    raise OptionParser::MissingArgument
 end
 
 # Default application name to the basename of the directory specified.
 dir = ARGV[0]
 unless options[:application]
-	options[:application] = File.basename(dir)
+    options[:application] = File.basename(dir)
 end
 
 # Build timer
@@ -52,26 +52,21 @@ sv = Svstat.new(dir)
 ps = Ppid.new()
 st = Statsd.new()
 
-
 # Main
 loop do
-	# sv.pid returns the process id for a daemontools managed application
-	# by querying it's status file.
+    # sv.pid returns the process id for a daemontools managed application
+    # by querying it's status file.
 
-	# ps.total returns the total amount of fds opened by all children 
-	# processes of sv.pid
-	fds = ps.total(sv.pid)
+    # ps.total returns the total amount of fds opened by all children 
+    # processes of sv.pid
+    fds = ps.total(sv.pid)
 
-	# Send the timer.  Not sure of the statsd protocol for timers, so
-	# sending it in the form "application.open_fd:fds|c"
-	st.send(mytimer, fds)
+    # Send the timer: ps.fds iterates over the number of open fds by child 
+    # process of sv.pid
+    ps.fds(sv.pid) do |fds|
+        st.send(mytimer, fds)
+    end
 
-	# Or for each child process ps.fds iterates over the number of open
-	# fds by child process of sv.pid
-	# ps.fds(sv.pid) do |fds|	
-	#	st.send(mytimer, fds)
-	# end
-
-	# Delay for interval
-	sleep(options[:interval])
+    # Delay for interval
+    sleep(options[:interval])
 end
