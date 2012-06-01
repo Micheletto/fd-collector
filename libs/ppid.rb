5471bb554afcce4ae4f5e5ac8b7438dbe8f6325d
Robert Micheletto <bobm@mozilla.com>
2012-06-01 09:30:47 -0700

    Fixed tabs

--- libs/ppid.rb
+++ libs/ppid.rb
@@ -2,58 +2,57 @@
 # ppids for a given process and Sys-Utils is old, so rolling a class to 
 # do it.
 class Ppid
-	# iterate over children PIDs
-	def each(pid)
-		# Include parent pid
-		yield pid
-		
-		# Named regex to make sure we have a number.
-		pidre = /[0-9]+/
+    # iterate over children PIDs
+    def each(pid)
+        # Include parent pid
+        yield pid
+        
+        # Named regex to make sure we have a number.
+        pidre = /[0-9]+/
 
-		# Assuming this is being run on a modern linux variant that
-		# has -ppid flag.
-		ppids = %x(/bin/ps --ppid #{pid} -o pid)
+        # Assuming this is being run on a modern linux variant that
+        # has -ppid flag.
+        ppids = %x(/bin/ps --ppid #{pid} -o pid)
 
-		# Will change this to raise an exception in the future and
-		# handle that by rechecking for the parent process, but just
-		# dying for now.
-		if(! $?.to_i.zero?):
-			puts "ps command failed."
-			exit 1
-		end
-		
-		# Yield only valid PIDs to be iterated over.
-		ppids.each do |p|
-			mypid = pidre.match(p) 
-			if mypid
-				yield mypid
-			end
-		end
-	end
+        # Will change this to raise an exception in the future and
+        # handle that by rechecking for the parent process, but just
+        # dying for now.
+        if(! $?.to_i.zero?):
+            puts "ps command failed."
+            exit 1
+        end
+        
+        # Yield only valid PIDs to be iterated over.
+        ppids.each do |p|
+            mypid = pidre.match(p) 
+            if mypid
+                yield mypid
+            end
+        end
+    end
 
-	# Iterate over number of FDs opened by children processes.
-	def fds(pid)
-		self.each(pid) do |p|
-			mydir = Dir.new("/proc/#{p}/fd")
-			myfiles = mydir.entries
-			mydir.close # Close this to avoid opening too many
-				    # FDs ourself.
+    # Iterate over number of FDs opened by children processes.
+    def fds(pid)
+        self.each(pid) do |p|
+            mydir = Dir.new("/proc/#{p}/fd")
+            myfiles = mydir.entries
+            mydir.close # Close this to avoid opening too many FDs ourself.
 
-			# Instead of using a pattern to ignore dot files, or
-			# match files comprising only of numbers, just subtract
-			# 2 from the total.  It's faster.
-			yield myfiles.length - 2
-		end
-	end
+            # Instead of using a pattern to ignore dot files, or
+            # match files comprising only of numbers, just subtract
+            # 2 from the total.  It's faster.
+            yield myfiles.length - 2
+        end
+    end
 
-	# Return the total of all FDs opened by children processes of pid.
-	def total(pid)
-		tot = 0
-		self.fds(pid) do |n|
-			tot += n
-		end
+    # Return the total of all FDs opened by children processes of pid.
+    def total(pid)
+        tot = 0
+        self.fds(pid) do |n|
+            tot += n
+        end
 
-		# Return this value.
-		tot
-	end
+        # Return this value.
+        tot
+    end
 end
--- libs/svstat.rb
+++ libs/svstat.rb
@@ -1,26 +1,26 @@
 # Class to return PID information from daemontools status files.
 # YMMV, you may need to check tai.h for format information.
 class Svstat
-	# Identify status file.
-	def initialize(dir)
-		@statfile = dir + "/supervise/status"
-	end
+    # Identify status file.
+    def initialize(dir)
+        @statfile = dir + "/supervise/status"
+    end
 
-	# Provid PID information.
-	def pid
-		# Open status file for binary reading.
-		f = File.open(@statfile, "rb:binary")
-		
-		# Seek to where PID information is.
-		f.seek(12)
-	
-		# Read four bytes.
-		bytes = f.read(4)
+    # Provid PID information.
+    def pid
+        # Open status file for binary reading.
+        f = File.open(@statfile, "rb:binary")
 
-		# Unpack unsigned integer.
-		data = bytes.unpack("i*")
-	
-		f.close
-		return(data)
-	end
+    # Seek to where PID information is.
+    f.seek(12)
+
+    # Read four bytes.
+    bytes = f.read(4)
+
+    # Unpack unsigned integer.
+    data = bytes.unpack("i*")
+
+    f.close
+    return(data)
+    end
 end
