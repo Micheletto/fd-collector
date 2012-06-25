ac6641504fc5752198bc25ded1a5548e45b227be
Robert Micheletto <bobm@mozilla.com>
2012-06-25 15:41:38 -0700

    Minor comment change

--- lib/FDcollect/StatdSend.rb
+++ lib/FDcollect/StatdSend.rb
@@ -4,9 +4,8 @@ require "socket"
 # Class to make UDP connections to statsd, and send timers
 class Statsd
 
-    # Open connection to host and port, and initialize the hash
-    # for assigning the proper facility / priority.  Use sensible
-    # defaults of localhost and port 514.
+    # Open connection to host and port.  Use sensible
+    # defaults of localhost and port 8125
     def initialize(host="127.0.0.1", port="8125")
         @usocket = UDPSocket.new
         @usocket.connect(host, port)
@@ -14,7 +13,6 @@ class Statsd
 
     # Deliver timer to statsd
     def send (timer, data)
-
         # Send the message to statsd.
         @usocket.send(timer + ":" + data.to_s + "|ms", 0)
     end
