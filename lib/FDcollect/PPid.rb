ecbc5bf448137c42e4fad738ad411a6b74979cfe
Robert Micheletto <bobm@mozilla.com>
2012-06-25 15:40:35 -0700

    Changed from ps to parsing /proc

--- lib/FDcollect/PPid.rb
+++ lib/FDcollect/PPid.rb
@@ -2,33 +2,51 @@
 # ppids for a given process and Sys-Utils is old, so rolling a class to 
 # do it.
 class Ppid
+    # Using Enumerable to add grep method to file class.
+    include Enumerable
+
+        # Named regex to make sure we have a number.
+        pidre = /[0-9]+/
+
+    # iterate over procfs, and return currently running processes
+    def ps
+
+        # Named regex to make sure we have a number.
+        pidre = /[0-9]+/
+
+        pdir = Dir.new("/proc")
+        myprocs = pdir.entries
+        pdir.close
+
+        myprocs.grep(pidre).each do |p|
+            yield p
+        end
+    end
+    
+
     # iterate over children PIDs
     def each(pid)
         # Include parent pid
         yield pid
         
-        # Named regex to make sure we have a number.
-        pidre = /[0-9]+/
+        # Named expression built around parent process that is being queried.
+        ppidre = /PPid:\s+#{pid}$/
 
-        # Assuming this is being run on a modern linux variant that
-        # has -ppid flag.  Procfs does not appear to list children processes,
-        # but parent process is available as the PPid flag in 'status'.  
-        # This should be converted to do that rather than shelling out to ps.
-        ppids = %x(/bin/ps --ppid #{pid} -o pid)
-
-        # Will change this to raise an exception in the future and
-        # handle that by rechecking for the parent process, but just
-        # dying for now.
-        if(! $?.to_i.zero?):
-            puts "ps command failed."
-            exit 1
-        end
-        
         # Yield only valid PIDs to be iterated over.
-        ppids.each do |p|
-            mypid = pidre.match(p) 
-            if mypid
-                yield mypid
+        self.ps do |p|
+            begin   # Processes appear and disappear constantly on a running
+                    # operating system.  Ignoring them as they disappear out
+                    # from under the application by using an empty rescue
+                    # block.
+
+            # Open status file, and look for our parent process.
+            myfile = File.new("/proc/#{p}/status", 'r')
+            unless(myfile.grep(ppidre).empty?):
+                yield p
+            end
+            myfile.close
+
+            rescue # Rescue by doing nothing.
             end
         end
     end
@@ -36,14 +54,25 @@ class Ppid
     # Iterate over number of FDs opened by children processes.
     def fds(pid)
         self.each(pid) do |p|
-            mydir = Dir.new("/proc/#{p}/fd")
-            myfiles = mydir.entries
-            mydir.close # Close this to avoid opening too many FDs ourself.
 
-            # Instead of using a pattern to ignore dot files, or
-            # match files comprising only of numbers, just subtract
-            # 2 from the total.  It's faster.
-            yield myfiles.length - 2
+            begin # Using begin and rescue block to handle disappearing 
+                  # processes, as in the each method.
+                    
+                mydir = Dir.new("/proc/#{p}/fd")
+                myfiles = mydir.entries
+                mydir.close # Close this to avoid opening too many FDs ourself.
+
+                # Instead of using a pattern to ignore dot files, or
+                # match files comprising only of numbers, just subtract
+                # 2 from the total.  It's faster.
+                yield myfiles.length - 2
+             
+            # If the process disappears yield the proper number of open file
+            # handles, which is now zero.
+            rescue
+                yield 0
+            end
+
         end
     end
 
