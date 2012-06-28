67b0ab12b4537bd031f015bebe196de0790ecd2f
Richard Soderberg <rsoderberg@gmail.com>
2012-06-27 17:24:41 -0700

    add MPL2 header to *.rb

--- lib/fdcollect/ppid.rb
+++ lib/fdcollect/ppid.rb
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 # The standard Process library doesn't contain a method for getting
 # ppids for a given process and Sys-Utils is old, so rolling a class to
 # do it.
